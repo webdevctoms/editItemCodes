2ff5fb77b5a064c04397b0b038dded25ee178990
Michael Peterson <WebDev@ctoms.ca>
2019-05-06 10:01:01 -0600

    initialcsv file read setup

--- js/index.js
+++ js/index.js
@@ -1,10 +1,59 @@
-function App(){
-
+function App(dropZoneID,downloadID){
+	this.csvDropZone = document.getElementById(dropZoneID);
+	this.downloadLink = document.getElementById(downloadID);
+	this.commaSplitData;
+	this.captureCSV = new CaptureCSV();
 }
 
 App.prototype.initApp = function() {
-	console.log("initApp");
+	this.csvDropZone.addEventListener("drop",function(e){
+		e.preventDefault();
+		this.fileDropped(e);
+	}.bind(this),false);
+
+	//need this to prevent default downloading of file
+	this.csvDropZone.addEventListener("dragover",function(e){
+		e.preventDefault();
+	}.bind(this),false);
+	//console.log("start app");
 };
 
-let app = new App();
+App.prototype.createCSV = function(arr){
+	let lineArray = [];
+	//console.log(arr);
+	arr.forEach(function(rowArr,index){
+		let row = rowArr.join("");
+		lineArray.push(index == 0 ? "data:text/csv;charset=utf-8," + row:row);	
+		//lineArray.push(row);
+	});
+	let csvContent = lineArray.join("\n");
+	let encodedUri = encodeURI(csvContent);
+	//console.log(csvContent);
+	return encodedUri
+};
+
+App.prototype.createDownload = function(csvData){
+	this.downloadLink.classList.remove("hide");
+	this.downloadLink.setAttribute("href","");
+	this.downloadLink.setAttribute("href",csvData);
+	this.downloadLink.setAttribute("download", "new_data.csv");
+};
+
+App.prototype.fileDropped = function(event){
+	let csvFile = event.dataTransfer.items[0].getAsFile();
+	this.captureCSV.readFile(csvFile)
+
+	.then(commaSplitData => {
+		this.commaSplitData = commaSplitData;
+		console.log(this.commaSplitData);
+		
+	})
+
+	.catch(err => {
+		console.log("error reading file", err);
+	});
+	//console.log(this.commaSplitData);
+};
+
+let app = new App("drop_zone","downloadLink");
 window.onload = app.initApp();
